cb3c1d40d17f1904c40a2540fd918bcdc05339c1
Sungkwan Kim <40896320+rlatjdrhks74@users.noreply.github.com>
2019-08-30 14:07:49 +0900

    첫 시작화면 시작버튼 깜빡임

---
+++ Buttonflick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttonflick : MonoBehaviour
+{
+    public Renderer[] joystick;
+    public Color OriginColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OriginColor = joystick[0].material.color; //조이스틱 칼라정보 저장
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //색 점멸
+        float flicker = Mathf.Abs(Mathf.Sin(Time.time * 4));
+        joystick[0].material.color = OriginColor * flicker * 2;
+        joystick[1].material.color = OriginColor * flicker * 2;
+        if (OVRInput.Get(OVRInput.Button.One) || OVRInput.Get(OVRInput.Button.Two))
+            SceneManager.LoadScene("Outside");
+    }
+}
