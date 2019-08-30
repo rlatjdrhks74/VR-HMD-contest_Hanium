37a3070060de83cde21f19d062468b60c3fc6364
Sungkwan Kim <40896320+rlatjdrhks74@users.noreply.github.com>
2019-08-30 14:05:28 +0900

    전원버튼 컨트롤러로 누름 -> 씬 전환하며 전동휠체어 충전완료

---
+++ PowerSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PowerSwitch : MonoBehaviour
+{
+    AudioSource Audioplayer;
+    public AudioClip Beep;           //소리
+    void OnTriggerEnter(Collider col)
+    {
+        Audioplayer = GetComponent<AudioSource>();
+        if (col.gameObject.name == "SwitchTrigger")
+        {
+            Audioplayer.PlayOneShot(Beep);
+            SceneManager.LoadScene("Inside");       //충전기 전원스위치 누르면 씬 전환할거임
+        }
+    }
+}
