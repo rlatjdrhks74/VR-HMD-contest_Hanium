90a661d7f8202bf4b7f6921512cc3c298860e39f
Sungkwan Kim <40896320+rlatjdrhks74@users.noreply.github.com>
2019-08-31 12:43:37 +0900

    PowerSwitch코드 업데이트 -> 기존 엘리베이터 버튼 누르는 방식 변경
    기존의 컨트롤러를 엘리베이터 버튼에 맞춰 UI를 띄워야했던 불편함을 해소하기 위해 단순히 오른쪽 손가락으로 누르면 엘리베이터 버튼을 누른것으로 인식하게 변경

---
+++ PowerSwitchAndElvDown.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PowerSwitchAndElvDown : MonoBehaviour
+{
+    AudioSource Audioplayer;
+    public AudioClip Beep;           //소리
+    private bool limit = true;
+    void OnTriggerEnter(Collider col)
+    {
+        Audioplayer = GetComponent<AudioSource>();
+        if (col.gameObject.name == "SwitchTrigger")
+        {
+            Audioplayer.PlayOneShot(Beep);
+            SceneManager.LoadScene("Inside");       //충전기 전원스위치 누르면 씬 전환할거임
+        }
+        if (col.gameObject.name == "DownButton" && limit == true)   //엘베버튼 누르면 내려감
+        {
+            limit = false;
+            GameObject.Find("DownButtonHand").SetActive(false);
+            Audioplayer.PlayOneShot(Beep);
+            col.gameObject.GetComponent<Button>().onClick.Invoke();
+        }
+        if (col.gameObject.name == "B2Button")   //엘베버튼 누르면 내려감
+        {
+            Audioplayer.PlayOneShot(Beep);
+            col.gameObject.GetComponent<Button>().onClick.Invoke();
+        }
+    }
+}
