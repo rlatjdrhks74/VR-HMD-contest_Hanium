f6e1e028de32c247c4f2c50dcad8b6f466df2c5e
Sungkwan Kim <40896320+rlatjdrhks74@users.noreply.github.com>
2019-09-04 11:39:41 +0900

    개찰구 앞 도착 -> 교통카드 등장

---
+++ GateEnter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateEnter : MonoBehaviour
+{
+    public GameObject Card;
+    // Start is called before the first frame update
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "CenterEyeAnchor")
+            Card.SetActive(true);
+    }
+}
