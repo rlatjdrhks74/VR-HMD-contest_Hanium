cc6ba57efa643b869bf245fc8cfb0a9a7b064928
Sungkwan Kim <40896320+rlatjdrhks74@users.noreply.github.com>
2019-09-04 11:39:10 +0900

    여러 오브젝트에 깜빡이는 효과

---
+++ Flicker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flicker : MonoBehaviour
+{
+    public Renderer joystick;
+    public Color OriginColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OriginColor = joystick.material.color; //조이스틱 칼라정보 저장
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //색 점멸
+        float flicker = Mathf.Abs(Mathf.Sin(Time.time * 4));
+        joystick.material.color = OriginColor * flicker * 2;
+    }
+}
